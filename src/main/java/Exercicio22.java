90557103a7f0e08e872ccc3a2f92057118ed1f98
root <root@DELL-7559>
2024-09-28 14:28:26 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio22.java
+++ src/main/java/Exercicio22.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Exercicio22 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int s = 0;
-		int e = 0;
-		int sum = 0;
-		int cnt = 0;
+      // DECLARATE
+      int s = 0;
+      int e = 0;
+      int sum = 0;
+      int cnt = 0;
 
-		// COLLECT DATA
-		System.out.print("START : ");
-		s = sc.nextInt();
-		System.out.print("END : ");
-		e = sc.nextInt();
+      // COLLECT DATA
+      System.out.print("START : ");
+      s = sc.nextInt();
+      System.out.print("END : ");
+      e = sc.nextInt();
 
-		// OUTPUT
-		for (int i = s; i <= e; i++) {
-			sum += i;
-			cnt++;
-		}
+      // OUTPUT
+      for (int i = s; i <= e; i++) {
+         sum += i;
+         cnt++;
+      }
 
-		System.out.print("AVERAGE: " + (sum / cnt));
-	}
+      System.out.print("AVERAGE: " + (sum / cnt));
+   }
 }
